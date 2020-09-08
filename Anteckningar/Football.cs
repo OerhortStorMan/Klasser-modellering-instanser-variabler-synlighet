249fbc6e394e73d4267e59086f2cc56f5e28fb46
lovewinelid <love.winelid@elev.ga.ntig.se>
2020-09-08 11:01:33 +0200

    Anteck.

---
+++ Anteckningar/Football.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Football
+{ //endast variabler och metoder, inga if while for etc
+    public float x;
+    public float y;
+    public float z;
+}
+
--- Anteckningar/Program.cs
+++ Anteckningar/Program.cs
@@ -6,9 +6,15 @@ namespace Anteckningar
     {
         static void Main(string[] args)
         {
+            Football theBall = new Football();
+            Football otherBall = new Football();
 
-            
 
+            theBall.x = 100;
+
+            System.Console.WriteLine(otherBall.x);
+
+            Console.ReadLine();
         }
     }
 }
